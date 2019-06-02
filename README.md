bb69bfd3435b29df039c723f4da57726a1a52316
chenzw <656469722@qq.com>
2019-06-02 21:06:46 +0800

    initialization

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 # servlet-samples
 
-**启动命令**
-> mvn tomcat7:run
+> 启动命令   
+
+$ mvn tomcat7:run
 
 ## servlet-jsp-sample
 
 > 访问地址
 
 http://localhost:8080/servlet-jsp-samples/userServlet    
-http://localhost:8080/servlet-jsp-samples/index.jsp
+http://localhost:8080/servlet-jsp-samples/index.jsp  
+http://localhost:8080/servlet-jsp-samples/home
+
+## servlet-annotation-samples
+
+> 访问地址
+
+http://localhost:8080/servlet-annotation-samples/home
---
+++ servlet-annotation-samples/src/main/java/HomeServlet.java
@@ -0,0 +1,26 @@
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "homeServlet", urlPatterns = {"/home"})
+public class HomeServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.sendRedirect(this.getServletContext().getContextPath() + "/index.jsp");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+}
